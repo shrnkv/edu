99de0d6d6b7ad896763135f8d060e50af3028572
shrnkv <a.shurenkov@ya.ru>
2022-03-20 17:28:24 +0300

    step 1

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// testgit
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieBD = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieBD.movies[a] = b;
+personalMovieBD.movies[c] = d;
+
+console.log(personalMovieBD);
