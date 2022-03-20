979a344124b288b1f500ea4b13a90de2274c28c3
shrnkv <a.shurenkov@ya.ru>
2022-03-20 18:02:28 +0300

    step 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieBD = {
     count: numberOfFilms,
@@ -8,12 +8,27 @@ const personalMovieBD = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
 
-personalMovieBD.movies[a] = b;
-personalMovieBD.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieBD.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieBD.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieBD.count >= 10 && personalMovieBD.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieBD.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieBD);
